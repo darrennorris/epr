29eea05864101901b0570945c4658dfed3ed9d8a
Darren Norris <doon75@hotmail.com>
2023-12-17 15:06:36 -0300

    include graphics chapter.

--- R/render_book.R
+++ R/render_book.R
@@ -1,12 +1,14 @@
 # bookdown
-bookdown::render_book("index.Rmd")
+bookdown::render_book("index.Rmd", "bookdown::html_document2")
+bookdown::render_book("index.Rmd", "bookdown::gitbook")
+
 # check
 browseURL("docs/index.html")
 # pdf, when using 
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 
 # quarto::quarto_render()
-# to use unicode and Portuguese need xelatex engine plus
+# to use unicode and Portuguese probably need xelatex engine plus
 # \usepackage{polyglossia}
 # \setmainlanguage{portuges}
 # in tinytext need hyphen-portuges and hyphen-portuguese
